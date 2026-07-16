7fb5a0325e5e507ace4b44d2cd67577009db79d2
ADI10YOGII <adiyogi.10ben@gmail.com>
2026-07-16 11:54:03 +0530

    Add files via upload

---
+++ NextGreatest.java
@@ -0,0 +1,38 @@
+package leet.Arrays;
+
+public class NextGreatest 
+{
+	public static void print(int[] arr)
+	{
+		
+		for(int ele:arr)
+		{
+			System.out.print(ele+" ");
+		}
+	}
+
+	public static void main(String[] args) 
+	{
+		int[] arr= {12,8,41,37,2,49,16,28,21};
+		int n=arr.length;
+		int[] ans=new int[n];
+		ans[n-1]=-1;
+		print(arr);
+		System.out.println();
+		
+		
+		for(int i=0;i<n-1;i++)
+		{
+			int mx=Integer.MIN_VALUE;
+			for(int j=i+1;j<n;j++)
+			{
+				mx=Math.max(mx, arr[j]);
+			}
+			ans[i]=mx;
+		}
+		print(ans);
+		// TODO Auto-generated method stub
+
+	}
+
+}
