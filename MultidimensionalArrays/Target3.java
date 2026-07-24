4c007718bb2f93b2b3889aab4444e95ba29a5460
ADI10YOGII <adiyogi.10ben@gmail.com>
2026-07-24 23:53:11 +0530

    Add files via upload

---
+++ MultidimensionalArrays/Target3.java
@@ -0,0 +1,47 @@
+package leet.MultiDimensionalArray;
+
+import java.util.Scanner;
+
+public class Target3 
+{
+	public static boolean find(int[][] arr,int target)
+	{
+		int m=arr.length;
+		int n=arr[0].length;
+		
+		int i=m-1;
+		int j=0;
+		
+		while(i<m&&j<n)
+		{
+			if(arr[i][j]==target)
+				return true;
+			else if(arr[i][j]>target)
+			{
+				i--;
+			}
+			else
+			{
+				j++;
+			}
+		}
+		
+
+		return false;
+	}
+
+	public static void main(String[] args) 
+	{
+		int[][] arr= {{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24}};
+		Scanner sc=new Scanner(System.in);
+		int target=sc.nextInt();
+		
+		System.out.println(find(arr,target));
+		
+		sc.close();
+	}
+		// TODO Auto-generated method stub
+
+	
+
+}
