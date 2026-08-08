78cb554d2523d1023e934570c3cf7325fa650cb4
ADI10YOGII <adiyogi.10ben@gmail.com>
2026-08-08 23:53:16 +0530

    Add files via upload

---
+++ Strings/Maximum1.java
@@ -0,0 +1,20 @@
+package leet.Strings;
+
+public class Maximum1 {
+
+	public static void main(String[] args) 
+	{
+		String[] arr= {"455","746745","56354","0025"};
+		
+		int mx=Integer.parseInt(arr[0]);
+		
+		for(int i=0;i<arr.length;i++)
+		{
+			int n=Integer.parseInt(arr[i]);
+			mx=Math.max(mx,n);
+		}
+		System.out.println(mx);
+
+	}
+
+}
