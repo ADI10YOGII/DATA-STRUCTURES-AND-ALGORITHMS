f3e253f699fa7a4018f47c48da9192156a1aeb88
ADI10YOGII <adiyogi.10ben@gmail.com>
2026-08-10 08:13:36 +0530

    Add files via upload

---
+++ Strings/IntegerToString.java
@@ -0,0 +1,25 @@
+package leet.Strings;
+import java.util.*;
+
+public class IntegerToString 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		
+		int n=sc.nextInt();
+		
+		String s="";
+		
+		s+=n;
+		
+		System.out.print(s+" ");
+		
+		sc.close();
+		
+		
+
+	}
+
+}
