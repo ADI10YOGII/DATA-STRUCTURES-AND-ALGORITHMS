9f9616423b69a85e3ef99349ef181ac56719168c
ADI10YOGII <adiyogi.10ben@gmail.com>
2026-08-12 20:12:16 +0530

    Add files via upload

---
+++ Strings/Compression.java
@@ -0,0 +1,44 @@
+package leet.Strings;
+
+public class Compression 
+{
+
+	public static void main(String[] args) 
+	{
+		String s="aaaaabbbccdeeff";
+		
+		char[] arr=s.toCharArray();
+		String ans="";
+		
+		int i=0,j=0;
+		while(j<arr.length)
+		{
+			if(arr[j]==arr[i])
+				j++;
+			else
+			{
+				ans+=arr[i];
+				
+				int len=j-i;
+				if(len>1)
+				{
+					ans+=len;
+				}
+				i=j;
+				
+			}
+		}
+		ans+=arr[i];
+		int len=j-i;
+		if(len>1)
+		{
+			ans+=len;
+			
+		}
+		System.out.println(ans);
+		
+		
+
+	}
+
+}
