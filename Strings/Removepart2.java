6e04276d5cafc964ffb2555fe6b2ab97691c44d5
ADI10YOGII <adiyogi.10ben@gmail.com>
2026-08-14 23:54:02 +0530

    Add files via upload

---
+++ Strings/Removepart2.java
@@ -0,0 +1,18 @@
+package leet.Strings;
+
+public class Removepart2 
+{
+
+
+	public static void main(String[] args) 
+	{
+		String a="aroan";
+		String b="aroanAdityaAnushka";
+		
+		String ans=b.replace(a,"");
+		System.out.println(ans);
+		
+
+	}
+
+}
