46aabdd4f192004849f76de3668190b5638399fb
ADI10YOGII <adiyogi.10ben@gmail.com>
2026-08-30 22:47:49 +0530

    Add files via upload

---
+++ Recursion/GCD.java
@@ -0,0 +1,28 @@
+package leet.Reccursion;
+
+import java.util.Scanner;
+
+public class GCD 
+{
+	public static int gcd(int a,int b)
+	{
+		if(b%a==0)
+			return a;
+		
+		return gcd(b%a,a);
+	}
+
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		
+		System.out.println(gcd(a,b));
+		
+		sc.close();
+		// TODO Auto-generated method stub
+
+	}
+
+}
