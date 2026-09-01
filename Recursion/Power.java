3431137c948fc0462995a2d60c3067fd0488835b
ADI10YOGII <adiyogi.10ben@gmail.com>
2026-09-01 22:18:11 +0530

    Add files via upload

---
+++ Recursion/Power.java
@@ -0,0 +1,26 @@
+package leet.Reccursion;
+import java.util.*;
+public class Power 
+{
+	public static int pow(int a,int b)
+	{
+		if (b==0)
+			return 1;
+		
+		return a*pow(a,b-1);
+		
+	}
+
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		
+		System.out.println(pow(a,b));
+		sc.close();
+		// TODO Auto-generated method stub
+
+	}
+
+}
