92cd7f71e6725c035f7e9f1beae64cbd51565c04
ADI10YOGII <adiyogi.10ben@gmail.com>
2026-09-23 22:24:52 +0530

    Add files via upload

---
+++ LinkedList2/CopyListRandomPointer.java
@@ -0,0 +1,106 @@
+package LinkedList2;
+
+public class CopyListRandomPointer 
+{
+	public static  Node deepCopy(Node head)
+	{
+		Node head2=new Node(head.val);
+		
+		Node t1=head.next;
+		Node t2=head2;
+		
+		while(t1!=null)
+		{
+			Node temp=new Node(t1.val);
+			t2.next=temp;
+			t2=t2.next;
+			t1=t1.next;
+		}
+		return head2;	
+	}
+	public static void connectAlternatively(Node head,Node head2)
+	{
+		Node t1=head;
+		Node t2=head2;
+		
+		Node dummy=new Node(-1);
+		Node t=dummy;
+		
+		while(t1!=null&&t2!=null)
+		{
+			t.next=t1;
+			t1=t1.next;
+			t=t.next;
+			t.next=t2;
+			t2=t2.next;
+			t=t.next;	
+		}
+	}
+	public static void assignRandom(Node head,Node head2)
+	{
+		Node t1=head;
+		Node t2=head2;
+		
+		while(t1!=null)
+		{
+			t2=t1.next;
+			if(t1.random!=null)
+			{
+				t2.random=t1.random.next;
+			}
+			t1=t1.next.next;
+		}
+	}
+	public static void split(Node head,Node head2)
+	{
+		Node t1=head;
+		Node t2=head2;
+		
+		while(t1!=null)
+		{
+			t1.next=t2.next;
+			t1=t1.next;
+			if(t1==null)
+				break;
+			t2.next=t1.next;
+			t2=t2.next;
+		}
+	}
+	public static Node copyRandomList(Node head)
+	{
+		if(head==null)
+			return null;
+		
+		//Creating a deep copy
+		Node head2=deepCopy(head);
+		
+		//Join head and head2 alternatively
+		connectAlternatively(head,head2);
+		
+		//Assign random pointers
+		assignRandom(head,head2);
+		
+		//split the linkedList
+		split(head,head2);
+		
+		return head2;
+	}
+
+	public static void main(String[] args) 
+	{
+		Node a=new Node(7);
+		Node b=new Node(13);
+		Node c=new Node(11);
+		Node d=new Node(10);
+		Node e=new Node(1);
+
+		
+		a.next=b;
+		b.next=c;
+		c.next=d;
+		d.next=e;
+		System.out.println(copyRandomList(a));
+
+	}
+
+}
